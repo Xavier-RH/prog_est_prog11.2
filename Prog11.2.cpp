5f3e5c6869b990d6c6e6ef7951f9d4810961ab01
Xavier Romero Hernández <100869394+Xavier-RH@users.noreply.github.com>
2022-05-22 11:35:41 -0500

    Add files via upload

---
+++ Prog11.2.cpp
@@ -0,0 +1,32 @@
+/* Autor: Xavier Romero Hernández, Realizado: 10/02/2022 
+	Programa para una tienda de Arduinos, en dicha Tienda existe una promocion
+		-Si se llevan minimo 5 arduinos, se le hace el 20% de descuento
+		-Si no, se le hace el 5% de descuento
+	Cada Arduino tiene un costo de $350.00
+	El programa pedira la cantidad de arduinos y calculara el total a pagar
+	*/
+
+#include <stdio.h>
+
+
+int main (){
+	int arduinos;
+	float total, descuento;
+	
+	printf("Introduce la cantidad de arduinos:");
+	scanf("%d",&arduinos);
+	
+	total=arduinos*350;
+	if(arduinos>=5){
+		descuento=total*0.20;
+	}
+	else{
+		descuento=total*0.05;
+	}
+	
+	printf("\nSubtotal:\t $%10.2f\n",total);
+	printf("Descuento:\t-$%10.2f\n",descuento);
+	printf("\t\t-------------\n");
+	printf("Total:\t\t $%10.2f",total-descuento);
+	return 0;
+}
